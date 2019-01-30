7c083bad0be7923cbae82464a7a46d0218b7e765
ruki <waruqi@gmail.com>
2019-01-30 23:36:57 +0800

    restore conanfile.py

---
+++ test_package/conanfile.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake, tools, RunEnvironment
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "xmake"
+    requires = ("zlib/1.2.11@conan/stable")
+    build_requires = ("xmake_installer/2.2.3@conan/stable")
+
+    def build(self):
+        self.run("xmake -P test_package")
+        
+    def test(self):
+        with tools.environment_append(RunEnvironment(self).vars):
+            self.run(os.path.join("bin", "test_package"))
